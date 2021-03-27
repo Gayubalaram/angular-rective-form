e4a39c2b56d4a8c9a186f2e06e3385f045308378
Gayubalaram <gayathribalaraman53@gmail.com>
2021-03-27 11:21:05 +0530

    angular forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl,FormGroup,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loginFrom= new FormGroup({
+    email: new FormControl('',Validators.required),
+    password: new FormControl(''),
+  })
+   get email()
+   {
+      return this.loginFrom.get('email')
+   }
+
+  collectData()
+  {
+    console.warn(this.loginFrom.value)
+  }
   title = 'forms';
 }
